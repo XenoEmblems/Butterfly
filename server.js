e7d2fdfb2b8e3f94ed1b2424c6c05af20ad03e0b
Eric Doyle <edoyle@drew.edu>
2016-05-16 14:22:35 -0400

    Thankyou page added

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var lesshat= require('lesshat');
 
 
 app.use(express.static(__dirname + '/public'));
@@ -9,6 +8,10 @@ app.use(express.static(__dirname + '/public'));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.get('/thankyou.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/thankyou.html'));
 });
 
 app.listen(8080);
